89024a99c65d7dcfa274ff4c3165da93df0004ec
Hiroki Kato <hirokikato1@gmail.com>
2023-09-01 07:20:34 -0400

    completes lab

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+-- INSERT INTO bears (name, age sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Sweet", 1)
+-- INSERT INTO bears (name, age sex, color, temperament, alive) VALUES ("Rowdy", 8, "F", "Blue", "Rambunctious", 1)
+-- INSERT INTO bears (name, age sex, color, temperament, alive) VALUES ("Tabitha", 7, "M", "Hazel", "Kind", 1)
+-- INSERT INTO bears (name, age sex, color, temperament, alive) VALUES ("Sargeant Brown", 6, "F", "Yellow", "Regimented", 1)
+-- INSERT INTO bears (name, age sex, color, temperament, alive) VALUES ("Melissa", 3, "M", "Gray", "Chatty", 1)
+-- INSERT INTO bears (name, age sex, color, temperament, alive) VALUES ("Grinch", 2, "F", "Red", "Grouchy", 1)
+-- INSERT INTO bears (name, age sex, color, temperament, alive) VALUES ("Wendy", 1, "M", "Black", "Explorative", 1)
+-- INSERT INTO bears (name, age sex, color, temperament, alive) VALUES (null, 5, "M", "Brown", "Mysterious", 1)
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sergeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
